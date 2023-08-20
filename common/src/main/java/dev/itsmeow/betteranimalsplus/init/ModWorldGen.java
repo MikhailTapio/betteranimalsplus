7a0e7e5f8375cefa3c01b294416cf99ee31778fe
Михаил Тапио <81358657+MikhailTapio@users.noreply.github.com>
2023-08-20 20:48:36 +0800

    Remove worldgen

--- common/src/main/java/dev/itsmeow/betteranimalsplus/init/ModWorldGen.java
+++ common/src/main/java/dev/itsmeow/betteranimalsplus/init/ModWorldGen.java
@@ -25,31 +25,6 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ModWorldGen {
-
-    public static ConfiguredFeature<RandomPatchConfiguration, ?> TRILLIUM_CF = null;
-    public static PlacedFeature TRILLIUM_PF = null;
-
     public static void init(Consumer<Runnable> enqueue) {
-        TRILLIUM_CF = new ConfiguredFeature(Feature.RANDOM_PATCH, FeatureUtils.simplePatchConfiguration(Feature.SIMPLE_BLOCK, new SimpleBlockConfiguration(
-                        new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.NORTH), 1)
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.EAST), 1)
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.SOUTH), 1)
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.WEST), 1))),
-                List.of(), 64));
-        TRILLIUM_PF = new PlacedFeature(Holder.direct(TRILLIUM_CF), List.of(
-                RarityFilter.onAverageOnceEvery(32),
-                InSquarePlacement.spread(),
-                HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING),
-                BiomeFilter.biome()));
-        enqueue.accept(() -> {
-            Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium"), TRILLIUM_CF);
-            Registry.register(BuiltinRegistries.PLACED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium"), TRILLIUM_PF);
-        });
-        BiomeModifications.addProperties(
-                ctx -> BiomeTypes.getTypes(ctx).contains(BiomeTypes.SWAMP),
-                (ctx, mutable) -> mutable.getGenerationProperties().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TRILLIUM_PF)
-        );
-
     }
 }
